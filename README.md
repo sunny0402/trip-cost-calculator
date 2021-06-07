1d9f3e660b4a8a1cc1af414ac7eb4fc21cbef3e7
sunny0402 <47863780+sunny0402@users.noreply.github.com>
2021-06-07 16:03:02 -0400

    add expense endpoint

---
+++ README.md
@@ -0,0 +1,54 @@
+## About
+
+GET: http://localhost:3000/trips
+
+```
+"trips": [
+{
+"_id": "60b93ec2dbf8a23480812b6b",
+"trip_name": "JavaScript Land"
+},
+{
+"_id": "60be6344c021d229384e88cd",
+"trip_name": "Turtles Island"
+}]}
+```
+
+POST: http://localhost:3000/expense
+
+```
+{
+    "trip": "60b93ec2dbf8a23480812b6b",
+    "date": "2018-07-22T20:22:13",
+    "amount": 100,
+    "category": "food",
+    "description": "A restuarant dinner."
+}
+```
+
+GET: http://localhost:3000/expenses
+Without parameter get all expenses.
+
+```
+//Request with parameter
+{
+    "trip": "60b93ec2dbf8a23480812b6b"
+}
+```
+
+```
+//Response with parameter
+{
+    "expenses": [
+        {
+            "_id": "60be790161037c48e4fbdcc6",
+            "trip": "60b93ec2dbf8a23480812b6b",
+            "date": "2018-07-22T20:22:13",
+            "amount": 100,
+            "category": "food",
+            "description": "A restuarant dinner."
+        }
+    ]
+}
+
+```
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const mongo = require("mongodb").MongoClient;
 const app = express();
 app.use(express.json());
 
 // windows: services start MongoDB Server
+//"C:\Program Files\MongoDB\Server\4.4\bin\mongo.exe"
 const mongo = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017";
 
@@ -28,7 +28,7 @@ mongo.connect(
 
 app.post("/trip", (req, res) => {
   /*
-  add trips. endpoint expects one param 
+  add trips endpoint expects one param 
   expect data as JSON via Content-Type: application/json
   */
   const name = req.body.name;
@@ -43,14 +43,51 @@ app.post("/trip", (req, res) => {
     res.status(200).json({ ok: true });
   });
 });
+
 app.get("/trips", (req, res) => {
-  /* */
+  /* list of trips in the trips collection */
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ the_error: err });
+      return;
+    }
+    res.status(500).json({ trips: items });
+  });
 });
+
+// POST /expense { trip, date, amount, category, description }
+// trip is the _id of one of the trips we previously added
 app.post("/expense", (req, res) => {
-  /* */
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description,
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json({ ok: true });
+    }
+  );
 });
+
+//accepts 1 parameter: _id property of a trip
 app.get("/expenses", (req, res) => {
-  /* */
+  expenses.find({ trip: req.body.trip }).toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ expenses: items });
+  });
 });
 
 app.listen(3000, () => console.log("Server ready"));
